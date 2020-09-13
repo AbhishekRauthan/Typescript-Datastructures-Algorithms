798a95a8a6c2a2df86ad1c56615d94e6cc24b19f
AbhishekRauthan <abhishek.rauthan.10@gmail.com>
2020-09-13 19:51:38 +0530

    Added Set.md and LinkedList.ts

---
+++ data-structures/LinkedList/LinkedList.ts
@@ -0,0 +1,84 @@
+interface LNode<T> {
+  value: T,
+  next: LNode<T> | null;
+}
+
+class LinkedList<T> {
+  private head: LNode<T>;
+  private tail: LNode<T>;
+  private len: number;
+
+  constructor(val: T) {
+    this.head = {
+      value: val,
+      next: null
+    }
+    this.tail = this.head;
+    this.len = 1;
+  }
+
+  append(val: T) {
+    const newNode: LNode<T> = {
+      value: val,
+      next: null
+    }
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.len++;
+  }
+
+  prepend(val: T) {
+    const newNode: LNode<T> = {
+      value: val,
+      next: null
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.len++;
+  }
+
+  gotoIndex(index: number): LNode<T> {
+    let counter = 0;
+    let currentNode: LNode<T> = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode
+  }
+
+  printList() {
+    const array: T[] = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    array.forEach((node: T) => {
+      console.log(node)
+    })
+  }
+
+  insert(val: T, index: number) {
+    if (index >= this.len) {
+      this.append(val)
+    }
+    const newNode: LNode<T> = {
+      value: val,
+      next: null
+    };
+    const leader = this.gotoIndex(index - 1)
+    let holdPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdPointer;
+    this.len++;
+  }
+
+  remove(index: number) {
+    const leader = this.gotoIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.len--;
+  }
+}
---
+++ data-structures/Set/Set.md
@@ -0,0 +1,7 @@
+# Set Data Structure
+
+## Defination
+
+<br/>
+
+### In computer science, a set is an abstract data type that can store unique values, without any particular order. Set’s are different from arrays in the sense that they only allow non-repeated, unique values within them.
