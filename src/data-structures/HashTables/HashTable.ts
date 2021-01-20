8d02d7549f07359329dc2190537aeeb6cc4c39c9
AbhishekRauthan <abhishek.rauthan.10@gmail.com>
2021-01-20 23:53:24 +0530

    done HashTable

--- src/data-structures/HashTables/HashTable.ts
+++ src/data-structures/HashTables/HashTable.ts
@@ -1,1 +1,37 @@
-  
+type VALUE<K> = [string, K]
+
+class HashTable<K> {
+  private data: VALUE<K>[][];
+  public constructor(size: number) {
+    this.data = new Array<VALUE<K>[]>(size);
+  }
+
+  private _hash(key: string) {
+    let hash = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+
+    return hash;
+  }
+
+  public set(key: string, value: K) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value])
+  }
+
+  public get(key: string) {
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    for (const bucket of currentBucket) {
+      if (bucket[0] === key) {
+        return bucket[1]
+      }
+    }
+    return false;
+  }
+}
